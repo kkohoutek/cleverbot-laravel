0a1ab0556e8b67cba94bd0dbf2192a0faadae1e5
Kamil Kohoutek <kamil008@seznam.cz>
2022-04-23 12:26:49 +0200

    Create Cleverbot.php

---
+++ src/Cleverbot.php
@@ -0,0 +1,31 @@
+<?php
+
+use GuzzleHttp\Client;
+
+class Cleverbot
+{
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->key = config('cleverbot.key');
+        $this->client = new Client([
+            'base_url' => 'https://www.cleverbot.com',
+        ]);
+    }
+
+
+    /**
+     * Return parsed json response as associative array.
+     *
+     * @return array
+     */
+    public function query($input = '', $cs = '', $cb_settings_tweak1 = '', $cb_settings_tweak2 = '', $cb_settings_tweak3 = '')
+    {
+        $key = config('cleverbot.key');
+        $response = $this->client->get('/getreply', [
+                'query' => compact($key, $input, $cs, $cb_settings_tweak1, $cb_settings_tweak2, $cb_settings_tweak3)
+        ]);
+        return (array) json_decode($response->getBody(), true);
+    }
+}
