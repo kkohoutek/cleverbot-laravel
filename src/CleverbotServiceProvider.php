9372ff14e5f17293ce45a1f5b1952d8ea03bc6e1
Kamil Kohoutek <kamil008@seznam.cz>
2022-04-23 12:27:17 +0200

    Create CleverbotServiceProvider.php

---
+++ src/CleverbotServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kkohoutek\Cleverbot;
+
+use Cleverbot;
+use Illuminate\Support\ServiceProvider;
+
+class CleverbotServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([__DIR__ . '../config/cleverbot.php' => config_path('cleverbot.php')]);
+    }
+
+    public function register()
+    {
+        $this->app->singleton(Cleverbot::class, fn () => new Cleverbot());
+    }
+}
